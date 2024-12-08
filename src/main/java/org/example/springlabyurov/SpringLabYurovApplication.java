b06da7d95c51267c7281a209fdf46b659d16aade
ElitePotatoes <elitepotatoman@gmail.com>
2024-12-08 13:08:59 +0300

    First

---
+++ src/main/java/org/example/springlabyurov/SpringLabYurovApplication.java
@@ -0,0 +1,13 @@
+package org.example.springlabyurov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLabYurovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLabYurovApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springlabyurov/SpringLabYurovApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springlabyurov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLabYurovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
